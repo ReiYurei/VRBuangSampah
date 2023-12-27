1c80432d363fe707fac0de38f041e85321da8f80
ReiYurei <106614665+ReiYurei@users.noreply.github.com>
2023-12-27 16:44:38 +0700

    Update Timer.cs

--- Assets/ReiYurei/Scripts/Timer.cs
+++ Assets/ReiYurei/Scripts/Timer.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class Timer : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public static float s_gameTimer;
+    public float gameTimer;
+    [SerializeField] private TMPro.TextMeshProUGUI timerText; //MANUALLY PUT
+    public static bool TimerTicking;
+
+    private string minutes;
+    private string seconds;
+
     void Start()
     {
-        
+        s_gameTimer = gameTimer;
+        minutes = Mathf.Floor(s_gameTimer / 60).ToString("00");
+        seconds = Mathf.Floor(s_gameTimer % 60).ToString("00");
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if (TimerTicking == true)
+        {
+            s_gameTimer -= Time.deltaTime;
+        }
+        minutes = Mathf.Floor(s_gameTimer / 60).ToString("00");
+        seconds = Mathf.Floor(s_gameTimer % 60).ToString("00");
+        timerText.text = $"{minutes} : {seconds}";
     }
 }

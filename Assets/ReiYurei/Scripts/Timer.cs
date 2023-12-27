30a0f6a71935ca91a8221bc2b947dff2f9730eb5
ReiYurei <106614665+ReiYurei@users.noreply.github.com>
2023-12-27 16:57:07 +0700

    Timer
    Added timer Events

--- Assets/ReiYurei/Scripts/Timer.cs
+++ Assets/ReiYurei/Scripts/Timer.cs
@@ -10,6 +10,9 @@ public class Timer : MonoBehaviour
     [SerializeField] private TMPro.TextMeshProUGUI timerText; //MANUALLY PUT
     public bool TimerTicking;
 
+    public delegate void TimerAction();
+    public static event TimerAction OnTimerEnd;
+
     private string minutes;
     private string seconds;
 
@@ -25,6 +28,11 @@ public class Timer : MonoBehaviour
         {
             TimerTicking = false;
             timerText.text = $" -- : -- ";
+            if (OnTimerEnd != null)
+            {
+                OnTimerEnd();
+            }
+
             return;
         }
         else if (TimerTicking == true)

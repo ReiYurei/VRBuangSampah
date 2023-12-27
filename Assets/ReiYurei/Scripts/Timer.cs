cf154c1687c7f2d43dd91dd6a3efc97047c6bf96
ReiYurei <106614665+ReiYurei@users.noreply.github.com>
2023-12-27 16:52:18 +0700

    Timer
    Added timer feature

--- Assets/ReiYurei/Scripts/Timer.cs
+++ Assets/ReiYurei/Scripts/Timer.cs
@@ -1,31 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Timer : MonoBehaviour
 {
-    public static float s_gameTimer;
-    public float gameTimer;
+    public float s_gameTimer;
+    //public float gameTimer;
     [SerializeField] private TMPro.TextMeshProUGUI timerText; //MANUALLY PUT
-    public static bool TimerTicking;
+    public bool TimerTicking;
 
     private string minutes;
     private string seconds;
 
     void Start()
     {
-        s_gameTimer = gameTimer;
+        //s_gameTimer = gameTimer;
         minutes = Mathf.Floor(s_gameTimer / 60).ToString("00");
         seconds = Mathf.Floor(s_gameTimer % 60).ToString("00");
     }
     void Update()
     {
-        if (TimerTicking == true)
+        if (s_gameTimer < 0)
+        {
+            TimerTicking = false;
+            timerText.text = $" -- : -- ";
+            return;
+        }
+        else if (TimerTicking == true)
         {
             s_gameTimer -= Time.deltaTime;
+            minutes = Mathf.Floor(s_gameTimer / 60).ToString("00");
+            seconds = Mathf.Floor(s_gameTimer % 60).ToString("00");
+            timerText.text = $"{minutes} : {seconds}";
         }
-        minutes = Mathf.Floor(s_gameTimer / 60).ToString("00");
-        seconds = Mathf.Floor(s_gameTimer % 60).ToString("00");
-        timerText.text = $"{minutes} : {seconds}";
+        
     }
 }
